3dce4941dff2908206de2cd0807caf1e4cd23eaf
Fernando Cavalcanti(DK) <comododk@gmail.com>
2022-05-03 10:06:21 -0300

    Feat: TransactionsTable map for table

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import  Modal  from 'react-modal';
 import buttonClose from '../../assets/buttonClose.svg';
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
@@ -19,12 +20,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
   function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault();
-    console.log(
+    const data = {
       title, 
       value, 
       category, 
       type
-    );
+    };
+
+    api.post('/transactions', data);
   }
 
   return (
